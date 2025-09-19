c57316bd4ce19398e756fb3bfddee26b53efeb46
Ekta Goyal <160715168+ektagoyal06@users.noreply.github.com>
2025-09-19 11:52:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 9898;
+
+app.get("/", (req, res)=> {
+    res.send("<h1>Hello from Express + Docker...!!!</h1>");
+})
+
+app.listen(PORT, () => {
+    console.log("Server started on PORT :",PORT);
+})
